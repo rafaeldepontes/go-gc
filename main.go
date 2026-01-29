2ee80b61526949144df62c90f9cd8663290b2dd3
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-01-29 14:13:52 -0300

    feat: using everything in main

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	gc "github.com/rafaeldepontes/go-gc/internal/gc"
+	obj "github.com/rafaeldepontes/go-gc/internal/object"
+)
+
+func main() {
+	vm := gc.NewVM()
+
+	a := vm.Heap.Alloc()
+	b := vm.Heap.Alloc(a)
+	c := vm.Heap.Alloc()
+
+	vm.Roots = []*obj.Object{b}
+
+	vm.GC()
+
+	fmt.Println(a.ID, b.ID, c.ID)
 }

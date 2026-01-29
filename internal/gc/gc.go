44ea456c64170f023aad173daef5d39b6d0ab50e
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-01-29 14:13:34 -0300

    feat: GC call and wait group

--- internal/gc/gc.go
+++ internal/gc/gc.go
@@ -12,22 +12,30 @@ var wg sync.WaitGroup
 // The logic here is pretty simple, if the logic is inside of the root
 // it should be still a live after the gc calls...
 type VM struct {
-	heap  *heap.Heap
-	roots []*object.Object
+	Heap  *heap.Heap
+	Roots []*object.Object
+}
+
+func NewVM() *VM {
+	return &VM{
+		Heap:  heap.NewHeap(),
+		Roots: make([]*object.Object, 0, 15),
+	}
 }
 
 // This is called tri-color marking, it goes from white to black blazing
 // fast!!
 func (vm *VM) markAll() {
-	for _, root := range vm.roots {
+	for _, root := range vm.Roots {
 		wg.Add(1)
 		go mark(root)
 	}
 }
 
-// returns the VM WaitGroup...
-func GetWG() *sync.WaitGroup {
-	return &wg
+func (vm *VM) GC() {
+	vm.markAll()
+	wg.Wait()
+	vm.Heap.Sweep()
 }
 
 // This is done recursively, not sure if it's the best choice... but is

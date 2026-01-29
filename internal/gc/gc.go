66d4e53e202a78d79bda723c136ad78cc4a7fbea
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-01-29 13:58:08 -0300

    feat: adding the gc logic

---
+++ internal/gc/gc.go
@@ -0,0 +1,44 @@
+package gc
+
+import (
+	"sync"
+
+	"github.com/rafaeldepontes/go-gc/internal/heap"
+	"github.com/rafaeldepontes/go-gc/internal/object"
+)
+
+var wg sync.WaitGroup
+
+// The logic here is pretty simple, if the logic is inside of the root
+// it should be still a live after the gc calls...
+type VM struct {
+	heap  *heap.Heap
+	roots []*object.Object
+}
+
+// This is called tri-color marking, it goes from white to black blazing
+// fast!!
+func (vm *VM) markAll() {
+	for _, root := range vm.roots {
+		wg.Add(1)
+		go mark(root)
+	}
+}
+
+// returns the VM WaitGroup...
+func GetWG() *sync.WaitGroup {
+	return &wg
+}
+
+// This is done recursively, not sure if it's the best choice... but is
+// the one I'm going for noow...
+func mark(obj *object.Object) {
+	defer wg.Done()
+	if obj.Mark {
+		return
+	}
+	obj.Mark = true
+	for _, ref := range obj.Refs {
+		mark(ref)
+	}
+}
